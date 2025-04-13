efc60e408c58d9a9b48ac2872557045891a76fb0
Aboubacardembandiaye2702 <146328820+Aboubacardembandiaye2702@users.noreply.github.com>
2025-04-13 06:39:15 +0000

    Update set.js

--- set.js
+++ set.js
@@ -1,15 +1,15 @@
 uconst dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  PREFIXE: process.env.PREFIXE || "#",
+  PREFIXE: process.env.PREFIXE || "!",
   NOM_OWNER: process.env.NOM_OWNER || "lugh",
   NUMERO_OWNER: process.env.NUMERO_OWNER || "221772430620",
   MODE: process.env.MODE || "public",
   MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
   SESSION_ID: process.env.SESSION_ID || "Ovl-MD_aUCZct6w_SESSION-ID",
   LEVEL_UP: process.env.LEVEL_UP || "non",
-  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "°•○haibodong○•°",
-  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "°•○F@T@L!T!○•°",
+  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "",
+  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "",
   DATABASE: process.env.DATABASE,
   RENDER_API_KEY: process.env.RENDER_API_KEY,
 };
